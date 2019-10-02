054095a5e7545f45193a9ae660194001938dd074
Lucas Craveiro <lucashcraveiro@gmail.com>
2019-10-02 16:13:22 -0700

    Added Models and Controllers

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const app = express();
+
+mongoose.connect(
+  'mongodb+srv://aircnc:yr159753@aircnc-kjl9d.mongodb.net/aircncproject?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
 // Get(get information), Post(add information), Put(edit), Delete
 // req.query - Access query params (for filters) - post
