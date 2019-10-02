c41d48ff6ede0c94cddcb787a90111be1190c990
Lucas Craveiro <lucashcraveiro@gmail.com>
2019-10-01 17:26:41 -0700

    Added routes.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
 
 // Get(get information), Post(add information), Put(edit), Delete
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello Brother' });
-});
+// req.query - Access query params (for filters) - post
+// req.params - Access route params (for edit or delete) - put
+// req.body - Access body of the requisition (for create and edit)
+
+app.use(express.json());
+app.use(routes);
+
 app.listen(3333);
