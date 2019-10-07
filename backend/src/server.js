64f8f21dd98c7154ab20d20de0ace866f5ceada5
Lucas Craveiro <lucashcraveiro@gmail.com>
2019-10-07 08:18:32 -0700

    frontend with react and changes on the server backend- added cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const routes = require('./routes');
 
 const app = express();
@@ -17,6 +19,7 @@ mongoose.connect(
 // req.params - Access route params (for edit or delete) - put
 // req.body - Access body of the requisition (for create and edit)
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,51 @@
-import React, { Component } from 'react';
+import React, {useState} from 'react';
+import api from './services/api';
 import './App.css';
+// eslint-disable-next-line
+import logo from './assets/logo.svg';
 
-class App extends Component {
-  render() {
-    return <h1>Hello World</h1>;
+function App() {
+
+  const [email, setEmail] = useState('');
+
+  async function handleSubmit (event) {
+
+    event.preventDefault();
+    console.log(email);
+
+    const response = await api.post('/sessions', {email});
+
+    const { _id} = response.data;
+
+    localStorage.setItem('user', _id);
+    
+    console.log(response);
+    console.log( _id);
   }
+
+  return (
+    <div className="container">
+      <img src={logo} alt="Aircnc" />
+
+      <div className="content">
+        <p>
+          Offer <strong>spots</strong> for programers and find new talents
+        </p>
+
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">E-mail *</label>
+          <input 
+            type="email" 
+            id="email" 
+            placeholder="type your email" 
+            value={email}
+            onChange={event=> setEmail(event.target.value)}
+          />
+          <button className="btn" type="submit">Go</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
