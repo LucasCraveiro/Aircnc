ab0a408b2ee49d88f0a9bafd2531d6385945467f
Lucas Craveiro <lucashcraveiro@gmail.com>
2019-10-08 08:06:36 -0700

    Changes on the back end to add thumbnail_url and frontend added functions to Dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,48 +1,19 @@
-import React, {useState} from 'react';
-import api from './services/api';
+import React from 'react';
 import './App.css';
 // eslint-disable-next-line
 import logo from './assets/logo.svg';
 
+import Routes from './routes';
+
 function App() {
 
-  const [email, setEmail] = useState('');
-
-  async function handleSubmit (event) {
-
-    event.preventDefault();
-    console.log(email);
-
-    const response = await api.post('/sessions', {email});
-
-    const { _id} = response.data;
-
-    localStorage.setItem('user', _id);
-    
-    console.log(response);
-    console.log( _id);
-  }
 
   return (
     <div className="container">
       <img src={logo} alt="Aircnc" />
 
       <div className="content">
-        <p>
-          Offer <strong>spots</strong> for programers and find new talents
-        </p>
-
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">E-mail *</label>
-          <input 
-            type="email" 
-            id="email" 
-            placeholder="type your email" 
-            value={email}
-            onChange={event=> setEmail(event.target.value)}
-          />
-          <button className="btn" type="submit">Go</button>
-        </form>
+        <Routes/>
       </div>
     </div>
   );
--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,6 +1,46 @@
-import React from 'react';
+import React, { useState} from 'react';
+import api from '../../services/api';
 
 
-export default function Login() {
-    return <div/>
+export default function Login({history}) {
+
+    const [email, setEmail] = useState('');
+
+        async function handleSubmit (event) {
+
+            event.preventDefault();
+            console.log(email);
+
+            const response = await api.post('/sessions', {email});
+
+            const { _id} = response.data;
+
+            localStorage.setItem('user', _id);
+
+            console.log(response);
+            console.log( _id);
+
+            history.push('/dashboard');
+        }
+
+    return (
+
+        <React.Fragment>
+            <p>
+            Offer <strong>spots</strong> for programers and find new talents
+            </p>
+
+            <form onSubmit={handleSubmit}>
+            <label htmlFor="email">E-mail *</label>
+            <input 
+                type="email" 
+                id="email" 
+                placeholder="type your email" 
+                value={email}
+                onChange={event=> setEmail(event.target.value)}
+            />
+            <button className="btn" type="submit">Go</button>
+            </form>
+        </React.Fragment>
+    )
 }
